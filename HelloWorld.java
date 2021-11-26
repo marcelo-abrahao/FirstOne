abd3143dbe9106cee2b91272cb4eeb9dae062e93
MarceloRTX <marcelo.abrahao@gmail.com>
2021-11-26 00:36:37 -0300

    Add package

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,8 @@
+import pkg.Order;
 public class HelloWorld {
     public static void main(String[] args) {
+        Order order = new Order("blzblzblzb");
         System.out.println("Hello World, now is the time !");
+        System.out.println(order);
     }
-
-}
+}   
